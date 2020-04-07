3716f939dd5750b935db8b090a44fda425fdeb74
ivang <silasevan@gmail.com>
2020-04-07 10:38:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function theBeatlesPlay(musicians,instruments){
 
   const musicians=["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
   const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-  for(i=0; i< musicians.length; i++) ;  (i< instruments.length){
+  for(i=0; i< musicians.length; i++) ;  (i< instruments.length)
     return (musicians + "play" + instruments);
-}
+
 }
 theBeatlesPlay(musicians,instruments)
